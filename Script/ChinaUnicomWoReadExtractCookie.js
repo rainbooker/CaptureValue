d4b4c438d817a5e434ee6f2c0e413a7da8d0cbe8
rainbooker <githubexplorer@outlook.com>
2024-05-27 13:55:15 +0800

    Update ChinaUnicomWoReadExtractCookie.js

--- Script/ChinaUnicomWoReadExtractCookie.js
+++ Script/ChinaUnicomWoReadExtractCookie.js
@@ -6,12 +6,12 @@ let cookie = $request.headers['Cookie'] || $request.headers['cookie'];
 // 检查是否成功获取到 Cookie
 if (cookie) {
   // 记录日志，换行后打印 Cookie 值
-  console.log("获取到的 Cookie:\n" + cookie);
+  console.log("联通App 阅光宝盒\n获取到的 Cookie:\n" + cookie);
   // 发送通知（可选）
-  $notify("Quantumult X", "Cookie 获取成功", cookie);
+  $notify("联通App 阅光宝盒", "Cookie 获取成功", cookie);
 } else {
-  console.log('没有获取到Cookie，刷新一下页面试试！');
-  $notify("Quantumult X", "没有获取到Cookie，刷新一下页面试试！");
+  console.log('联通App 阅光宝盒\n没有获取到Cookie，刷新一下页面试试！');
+  $notify("联通App 阅光宝盒", "没有获取到Cookie，刷新一下页面试试！");
 }
 
 // 结束响应处理并返回原始响应体
