770936fa63973deec0a2d2046b049b7a5611edca
rainbooker <githubexplorer@outlook.com>
2024-05-27 13:35:11 +0800

    Update HisenseExtractActivityCookie.js

--- Script/HisenseExtractActivityCookie.js
+++ Script/HisenseExtractActivityCookie.js
@@ -6,12 +6,12 @@ let cookie = $request.headers['Cookie'] || $request.headers['cookie'];
 // 检查是否成功获取到 Cookie
 if (cookie) {
   // 记录日志，换行后打印 Cookie 值
-  console.log("获取到的 Cookie:\n" + cookie);
+  console.log("海信爱家 公众号\n获取到做任务的 Cookie:\n" + cookie);
   // 发送通知（可选）
-  $notify("Quantumult X", "Cookie 获取成功", cookie);
+  $notify("海信爱家 公众号", "做任务Cookie 获取成功", cookie);
 } else {
-  console.log('没有获取到Cookie，刷新一下页面试试！');
-  $notify("Quantumult X", "没有获取到Cookie，刷新一下页面试试！");
+  console.log('海信爱家 公众号\n没有获取到做任务的Cookie，刷新一下页面试试！');
+  $notify("海信爱家 公众号", "没有获取到做任务的Cookie，刷新一下页面试试！");
 }
 
 // 结束响应处理并返回原始响应体
