3ffc3b2ee8dafcddd6ee2d3fd7189e29546d5226
rainbooker <githubexplorer@outlook.com>
2024-05-30 09:02:39 +0800

    Update MoonBoxExtractRequestBody.js

--- Script/MoonBoxExtractRequestBody.js
+++ Script/MoonBoxExtractRequestBody.js
@@ -6,9 +6,9 @@ let requestBody = $request.body;
 // 检查是否成功获取到 请求体
 if (requestBody) {
   // 记录日志，换行后打印 请求体
-  console.log("联通App 阅光宝盒\n获取到当前阅读任务的请求体:\n" + requestBody);
+  console.log("联通App 阅光宝盒\n获取到当前阅读任务 请求体:\n" + requestBody);
   // 发送通知（可选）
-  $notify("联通App 阅光宝盒", "当前阅读任务的请求体 获取成功", requestBody);
+  $notify("联通App 阅光宝盒", "当前阅读任务 请求体 获取成功", requestBody);
 } else {
   console.log('联通App 阅光宝盒\n没有获取到当前阅读任务的请求体，刷新一下页面试试！');
   $notify("联通App 阅光宝盒", "没有获取到当前阅读任务的请求体，刷新一下页面试试！");
